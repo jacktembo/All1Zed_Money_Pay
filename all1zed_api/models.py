b4f7a078b07f94b65343b2e42a829fdec74517be
Mukumbuta <mukumbutasimasiku@gmail.com>
2022-12-27 12:17:44 +0200

    add models

---
+++ all1zed_api/models.py
@@ -0,0 +1,103 @@
+from django.utils import timezone
+# from django.contrib.postgres.fields import JSONField
+from authentication.models import User
+from django.db import models
+from django.conf import settings
+
+
+class CardAccount(models.Model):
+    first_name = models.CharField(max_length=255, blank=True)
+    last_name = models.CharField(max_length=255, blank=True)
+    phone_number = models.CharField(max_length=16)
+    card_number = models.CharField(max_length=200, null=True, blank=True)
+    card_id = models.CharField(max_length=200, null=True, blank=True)
+    card_balance = models.FloatField(max_length=250, default=0)
+    is_active = models.BooleanField(default=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    
+    def card_ID_generator(self):
+        return random.randrange(000000000000000, 999999999999999)
+
+
+    def __str__(self):
+        return str(f'First name: {self.first_name} | Last name: {self.last_name}') 
+
+
+class BusinessAccount(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    business_name = models.CharField(max_length=200)
+    bank_account_number = models.CharField(max_length=200, blank=True)
+    account_type = models.CharField(max_length=100)# momo or bank
+    commission = models.CharField(max_length=10, default='') #Used on card tap
+    merchant_code = models.CharField(max_length=16, unique=True)
+    phone_number = models.CharField(max_length=16)
+    balance = models.FloatField(max_length=250, default=0)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(f'Business name: {self.business_name} | Balance: {self.balance}')
+    
+
+class Payment(models.Model):
+    service_provider = models.CharField(max_length=200, null=True, blank=True) # mtn, airtel, kazang, zesco, liquid
+    service = models.CharField(max_length=200, null=True, blank=True)  # TV, Water, zesco
+    phone_number = models.CharField(max_length=250, default=0)
+    txn_amount = models.FloatField(max_length=250, default=0)
+    card_number = models.CharField(max_length=250, default=0)
+    merchant_code = models.FloatField(max_length=250, default=0)
+    txn_charge = models.FloatField(max_length=250, default=0)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user_id}-{self.service_provider}-{self.service}"
+
+
+class UserWallet(models.Model):
+    # wallet_id = models.UUIDField(unique=True, default=uuid.uuidv4)
+    # payment = JSONField(null=True, blank=True)
+    created_on = models.DateTimeField(auto_now=True)
+
+
+class MerchantCode(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    merchant_code = models.CharField(max_length=15, unique=True)
+    business_name = models.CharField(max_length=200)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+
+class Transaction(models.Model):
+    branch_name = models.ForeignKey(BusinessAccount, on_delete=models.CASCADE, null=True, blank=True)
+    txn_type = models.CharField(max_length=100, blank=True, null=True) #momo or bank
+    txn_amount = models.CharField(max_length=1000, blank=True, null=True)
+    txn_charge = models.CharField(max_length=100, blank=True, null=True)
+    update_amount = models.CharField(max_length=1000, blank=True, null=True)
+    reference_id = models.CharField(max_length=200, blank=True)
+    message = models.CharField(max_length=1000, blank=True)
+    status = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.txn_action} - {self.reference_id}'
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+#    def __str__(self):
+#        return f"{self.username} - {self.trans_action} - {self.refference_id}"

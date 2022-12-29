1d26208a85beebea9e845aabde35a43a54c3372b
Mukumbuta <mukumbutasimasiku@gmail.com>
2022-12-29 14:45:31 +0200

    add bank cashin functionality

--- all1zed_api/cashin.py
+++ all1zed_api/cashin.py
@@ -26,7 +26,8 @@ from all1zed_api.nfs_cashin import (
     nfs_momo_cashin, 
     nfs_momo_cashin_confirm, nfs_airtel_cashin, 
     nfs_airtel_cashin_confirm, nfs_zamtel_cashin,
-    nfs_zamtel_cashin_confirm,
+    nfs_zamtel_cashin_confirm, nfs_zanaco_cashin,
+    nfs_zanaco_cashin_confirm,
 )
 from all1zed_api.send_notification import send_notification
 
@@ -104,11 +105,26 @@ class CashinView(APIView):
                     if sender and receiver:
                         txn_id = generate_pin()
                         Transaction.objects.create(
-                            phone_number=phone_number,
+                            card_number=sender.card_number,
+                            txn_type='transfer',
+                            account_type=account_type,
+                            txn_amount=amount,                      
+                            update_amount=sender.card_balance,             
+                            reference_id=txn_id,
+                            message=f'Transfer of {amount} to {receiver.card_number} successful',
+                            status= True
+                        ) 
+
+                        Transaction.objects.create(
                             card_number=card_number,
-                            status=True,
-                            reference_id=txn_id
-                        )
+                            txn_type='transfer',
+                            account_type=account_type,
+                            txn_amount=amount,                      
+                            update_amount=receiver.card_balance,             
+                            reference_id=txn_id,
+                            message=f'You received {amount} from {sender.card_number}.',
+                            status= True
+                        ) 
 
                         verification_msg  = f'Dear customer, you have sent ZMW{amount} to {receiver.first_name} {receiver.first_name} card account number {receiver_card_number}. Your card balance is now ZMW{sender.card_balance}. Txn ID: {txn_id}'
                         notification_msg = f'Dear customer, you have received ZMW{amount} from {sender.first_name} {sender.first_name} {sender.phone_number}. Your card balance is now ZMW{receiver.card_balance}'
@@ -118,76 +134,142 @@ class CashinView(APIView):
                         return Response({'Success': 'OK'})
                     return Response({"Error": serializer.errors})
 
-                elif account_type == 'mtn':
-                    momo_status = nfs_momo_cashin(amount, reference, login_session)
-                    try:
-                        if momo_status.get("response_code") == '0':
-                            print(f'MTN-APPROVAL-REPRINT {momo_status.get("response_code")}')
-                            confirmation_number = momo_status.get('confirmation_number')
-                            print(confirmation_number)
-                            confirm_response = nfs_momo_cashin_confirm(confirmation_number, login_session)
-                            sender.card_balance -= float(amount)
-                            sender.save()
-
-                            verification_msg  = f'Dear customer, you have transfered ZMW{amount} to {reference}. Your card balance is now ZMW{sender.card_balance}. Txn ID: {txn_id}'       
-                            send_notification(receiver.phone_number, verification_msg)
-                            print(f'APPROVAL-CONFIRM {confirm_response}')
-
-                            return Response({'Success':'OK', 'Data': serializer.data})
-                        return Response({'Error': True})
-                    except (KeyError, TypeError) as e:
-                        pass
-
-                elif account_type == 'airtel':
-                    momo_status = nfs_airtel_cashin(amount, reference, login_session)
-                    try:
-                        if momo_status.get("response_code") == '0':
-                            print(f'MTN-APPROVAL-REPRINT {momo_status.get("response_code")}')
-                            confirmation_number = momo_status.get('confirmation_number')
-                            print(confirmation_number)
-                            confirm_response = nfs_airtel_cashin_confirm(confirmation_number, login_session)
-                            sender.card_balance -= float(amount)
-                            sender.save()
-                            
-                            verification_msg  = f'Dear customer, you have transfered ZMW{amount} to {reference}. Your card balance is now ZMW{sender.card_balance}. Txn ID: {txn_id}'
-                            send_notification(sender.phone_number, verification_msg)
-                            print(f'Airtel-CONFIRM {confirm_response}')
-
-                            return Response({"Success": serializer.data, "Message": "Transfer successful."})
-                        return Response({'Error': True})
-                    except (KeyError, TypeError) as e:
-                        pass
-                
-                elif account_type == 'zamtel':
-                    momo_status = nfs_zamtel_cashin(amount, reference, login_session)
-                    try:
-                        if momo_status.get("response_code") == '0':
-                            print(f'MTN-APPROVAL-REPRINT {momo_status.get("response_code")}')
-                            confirmation_number = momo_status.get('confirmation_number')
-                            print(confirmation_number)
-                            confirm_response = nfs_zamtel_cashin_confirm(confirmation_number, login_session)
-                            sender.card_balance -= float(amount)
-                            sender.save()
-                            
-                            verification_msg  = f'Dear customer, you have transfered ZMW{amount} to {reference}. Your card balance is now ZMW{sender.card_balance}. Txn ID: {txn_id}'
-                            send_notification(sender.phone_number, verification_msg)
-                            print(f'Airtel-CONFIRM {confirm_response}')
-
-                            return Response({"Success": serializer.data, "Message": "Transfer successful."})
-                        return Response({'Error': True})
-                    except (KeyError, TypeError) as e:
-                        pass
+                elif account_type == 'mobile_money':
+                    institution_name = request.data.get('institution_name', None)
                     
+                    if institution_name == 'mtn':
+                        momo_status = nfs_momo_cashin(amount, reference, login_session)
+                        try:
+                            if momo_status.get("response_code") == '0':
+                                print(f'MTN-APPROVAL-REPRINT {momo_status.get("response_code")}')
+                                confirmation_number = momo_status.get('confirmation_number')
+                                print(confirmation_number)
+                                confirm_response = nfs_momo_cashin_confirm(confirmation_number, login_session)
+                                sender.card_balance -= float(amount)
+                                sender.save()
+
+                                Transaction.objects.create(
+                                    card_number=card_number,
+                                    txn_type='cashin',
+                                    account_type=account_type,
+                                    institution_name=institution_name,
+                                    txn_amount=amount,                      
+                                    update_amount=sender.card_balance,             
+                                    reference_id=txn_id,
+                                    message=f'Transfer of {amount} to {reference} successful',
+                                    status= True
+                                ) 
+                                verification_msg  = f'Dear customer, you have transfered ZMW{amount} to {reference}. Your card balance is now ZMW{sender.card_balance}. Txn ID: {txn_id}'       
+                                send_notification(receiver.phone_number, verification_msg)
+                                print(f'APPROVAL-CONFIRM {confirm_response}')
+
+                                return Response({'Success':'OK', 'Data': serializer.data})
+                            return Response({'Error': True})
+                        except (KeyError, TypeError) as e:
+                            pass
+
+                    elif institution_name == 'airtel':
+                        momo_status = nfs_airtel_cashin(amount, reference, login_session)
+                        try:
+                            if momo_status.get("response_code") == '0':
+                                print(f'MTN-APPROVAL-REPRINT {momo_status.get("response_code")}')
+                                confirmation_number = momo_status.get('confirmation_number')
+                                print(confirmation_number)
+                                confirm_response = nfs_airtel_cashin_confirm(confirmation_number, login_session)
+                                sender.card_balance -= float(amount)
+                                sender.save()
+                                
+                                Transaction.objects.create(
+                                    card_number=card_number,
+                                    txn_type='cashin',
+                                    account_type=account_type,
+                                    institution_name=institution_name,
+                                    txn_amount=amount,                      
+                                    update_amount=sender.card_balance,             
+                                    reference_id=txn_id,
+                                    message=f'Transfer of {amount} to {reference} successful',
+                                    status= True
+                                ) 
+                                verification_msg  = f'Dear customer, you have transfered ZMW{amount} to {reference}. Your card balance is now ZMW{sender.card_balance}. Txn ID: {txn_id}'
+                                send_notification(sender.phone_number, verification_msg)
+                                print(f'Airtel-CONFIRM {confirm_response}')
+
+                                return Response({"Success": serializer.data, "Message": "Transfer successful."})
+                            return Response({'Error': True})
+                        except (KeyError, TypeError) as e:
+                            pass
+                    
+                    elif institution_name == 'zamtel':
+                        momo_status = nfs_zamtel_cashin(amount, reference, login_session)
+                        try:
+                            if momo_status.get("response_code") == '0':
+                                print(f'MTN-APPROVAL-REPRINT {momo_status.get("response_code")}')
+                                confirmation_number = momo_status.get('confirmation_number')
+                                print(confirmation_number)
+                                confirm_response = nfs_zamtel_cashin_confirm(confirmation_number, login_session)
+                                sender.card_balance -= float(amount)
+                                sender.save()
+
+                                Transaction.objects.create(
+                                    card_number=card_number,
+                                    txn_type='cashin',
+                                    account_type=account_type,
+                                    institution_name=institution_name,
+                                    txn_amount=amount,                      
+                                    update_amount=sender.card_balance,             
+                                    reference_id=txn_id,
+                                    message=f'Transfer of {amount} to {reference} successful',
+                                    status= True
+                                ) 
+                                verification_msg  = f'Dear customer, you have transfered ZMW{amount} to {reference}. Your card balance is now ZMW{sender.card_balance}. Txn ID: {txn_id}'
+                                send_notification(sender.phone_number, verification_msg)
+                                print(f'Airtel-CONFIRM {confirm_response}')
+
+                                return Response({"Success": serializer.data, "Message": "Transfer successful."})
+                            return Response({'Error': True})
+                        except (KeyError, TypeError) as e:
+                            pass
+                    else:
+                        return Response({'Error': 'Please enter a valid institution name'})
+
+                elif account_type == 'bank':
+                    bank_name = request.data.get('bank_name', None).lower()
+
+                    if bank_name == 'zanaco':
+                        account_number = request.data.get('account_number', None).lower()
+                        reference = account_number
+                        
+                        try:
+                            disbursment = nfs_zanaco_cashin(f'{amount}00', reference, login_session)
+                            if disbursment.get("response_code") == '0':
+                                confirmation_number = disbursment.get('confirmation_number')
+                                confirm_response = nfs_zanaco_cashin_confirm(confirmation_number, login_session)
+                                sender.card_balance -= float(amount)
+                                sender.save()
+                                txn_id = generate_pin(12)
+                                print(f'Zanaco-CONFIRM {confirm_response}')
+
+                                Transaction.objects.create(
+                                    card_number=card_number,
+                                    txn_type='cashin',
+                                    account_type=account_type,
+                                    institution_name=bank_name,
+                                    txn_amount=amount,                      
+                                    update_amount=sender.card_balance,             
+                                    reference_id=txn_id,
+                                    message=f'Transfer of {amount} to {reference} - {bank_name} successful',
+                                    status= True
+                                )   
+                            
+                                notification_msg = f'You have successfully transfered ZMW{amount} to {reference}. Your balance is now {sender.card_balance} Transaction ID: {txn_id}.'
+                                send_notification(sender.phone_number, notification_msg)
+                                
+                                return Response({'Success': 'OK', 'Details': serializer.data})
+                            return Response({'Error': True})
+                        except (KeyError, TypeError) as e:
+                            pass
+                        
+                    return Response({"Error": 'Please enter a valid bank name.'})
                 return Response({"Error": 'Unknown account type'})
             return Response({'Error': 'Unknown phone number'})
         return Response({'Error': serializer.errors})
-
-
-#{
-#  "amount": "100",
-#   "phone_number": "0976637416",
-#   "account_reference": "0963533399",
-#  "account_type": "mtn",
-#   "session_uuid": "a529f7c9-9806-4533-86b3-53a68dbdb49c",
-#   "card_number": "0022"
-#}

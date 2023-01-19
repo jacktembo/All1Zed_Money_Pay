a9a2dcb50e6fa68b94c243a3c9f5387ef114b530
Mukumbuta <mukumbutasimasiku@gmail.com>
2023-01-19 11:09:06 +0200

    add routing path patterns

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,8 +6,8 @@ from .views import (
 
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name = 'register_user'),
-    path('login/', LoginView.as_view(), name = 'login'),
-    path('card/register/', RegisterCardView.as_view(), name='register_card'),
-    path('register/business', BusinessProfileView.as_view(), name='register_business')
+    path('acounts/register/', RegisterView.as_view(), name = 'register_user'),
+    path('accounts/login/', LoginView.as_view(), name = 'login'),
+    path('accounts/register/card/', RegisterCardView.as_view(), name='register_card'),
+    path('accounts/register/business', BusinessProfileView.as_view(), name='register_business')
 ]
